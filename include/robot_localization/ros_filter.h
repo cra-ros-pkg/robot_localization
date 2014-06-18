16de6503576bbc53f2f533f2d67a31f2400e3920
Pulse Developer <ayrton04@gmail.com>
2014-06-18 15:05:36 -0400

    Adding deprecation warning and handling for old differential setting

--- include/robot_localization/ros_filter.h
+++ include/robot_localization/ros_filter.h
@@ -237,6 +237,46 @@ namespace RobotLocalization
             bool differential;
             nhLocal_.param(odomTopicName + std::string("_differential"), differential, false);
 
+            ////////// HANDLE DEPRECATED DIFFERENTIAL SETTING //////////
+            if(!differential)
+            {
+              XmlRpc::XmlRpcValue diffConfig;
+
+              if (nhLocal_.hasParam(odomTopicName + std::string("_differential")))
+              {
+                try
+                {
+                  nhLocal_.getParam(odomTopicName + std::string("_differential"), diffConfig);
+
+                  if(diffConfig.getType() == XmlRpc::XmlRpcValue::TypeArray)
+                  {
+                    ROS_WARN_STREAM("Per-variable configuration of differential settings is deprecated. " <<
+                                    "Please set the value for " << odomTopicName << std::string("_differential") <<
+                                    " to \"true\" or \"false.\"");
+
+                    // If any of the variables is true, make the whole sensor differential
+                    for (int i = 0; i < diffConfig.size() && !differential; i++)
+                    {
+                      // The double cast looks strange, but we'll get exceptions if we
+                      // remove the cast to bool. vector<bool> is discouraged, so updateVector
+                      // uses integers
+                      differential = differential || static_cast<int>(static_cast<bool>(diffConfig[i]));
+                    }
+
+                    if(differential)
+                    {
+                      ROS_WARN_STREAM("Setting " << odomTopicName << std::string("_differential") << " to true");
+                    }
+                  }
+                }
+                catch(...)
+                {
+
+                }
+              }
+            }
+            ////////// END DEPRECATED DIFFERENTIAL SETTING //////////
+
             // Subscribe using boost::bind, which lets us append arbitrary data,
             // in this case, the topic name (e.g., odom0 or odom1)
             std::string odomTopic;
@@ -269,6 +309,46 @@ namespace RobotLocalization
           {
             bool differential;
             nhLocal_.param(poseTopicName + std::string("_differential"), differential, false);
+
+            ////////// HANDLE DEPRECATED DIFFERENTIAL SETTING //////////
+            if(!differential)
+            {
+              XmlRpc::XmlRpcValue diffConfig;
+
+              if (nhLocal_.hasParam(poseTopicName + std::string("_differential")))
+              {
+                try
+                {
+                  nhLocal_.getParam(poseTopicName + std::string("_differential"), diffConfig);
+
+                  if(diffConfig.getType() == XmlRpc::XmlRpcValue::TypeArray)
+                  {
+                    ROS_WARN_STREAM("Per-variable configuration of differential settings is deprecated. " <<
+                                    "Please set the value for " << poseTopicName << std::string("_differential") <<
+                                    " to \"true\" or \"false.\"");
+
+                    // If any of the variables is true, make the whole sensor differential
+                    for (int i = 0; i < diffConfig.size() && !differential; i++)
+                    {
+                      // The double cast looks strange, but we'll get exceptions if we
+                      // remove the cast to bool. vector<bool> is discouraged, so updateVector
+                      // uses integers
+                      differential = differential || static_cast<int>(static_cast<bool>(diffConfig[i]));
+                    }
+
+                    if(differential)
+                    {
+                      ROS_WARN_STREAM("Setting " << poseTopicName << std::string("_differential") << " to true");
+                    }
+                  }
+                }
+                catch(...)
+                {
+
+                }
+              }
+            }
+            ////////// END DEPRECATED DIFFERENTIAL SETTING //////////
 
             std::string poseTopic;
             nhLocal_.getParam(poseTopicName, poseTopic);
@@ -328,6 +408,47 @@ namespace RobotLocalization
             bool differential;
             nhLocal_.param(imuTopicName + std::string("_differential"), differential, false);
 
+            ////////// HANDLE DEPRECATED DIFFERENTIAL SETTING //////////
+            if(!differential)
+            {
+              XmlRpc::XmlRpcValue diffConfig;
+
+              if (nhLocal_.hasParam(imuTopicName + std::string("_differential")))
+              {
+                try
+                {
+                  nhLocal_.getParam(imuTopicName + std::string("_differential"), diffConfig);
+
+                  if(diffConfig.getType() == XmlRpc::XmlRpcValue::TypeArray)
+                  {
+                    ROS_WARN_STREAM("Per-variable configuration of differential settings is deprecated. " <<
+                                    "Please set the value for " << imuTopicName << std::string("_differential") <<
+                                    " to \"true\" or \"false.\"");
+
+                    // If any of the variables is true, make the whole sensor differential
+                    for (int i = 0; i < diffConfig.size() && !differential; i++)
+                    {
+                      // The double cast looks strange, but we'll get exceptions if we
+                      // remove the cast to bool. vector<bool> is discouraged, so updateVector
+                      // uses integers
+                      differential = differential || static_cast<int>(static_cast<bool>(diffConfig[i]));
+                    }
+
+                    if(differential)
+                    {
+                      ROS_WARN_STREAM("Setting " << imuTopicName << std::string("_differential") << " to true");
+                    }
+                  }
+                }
+                catch(...)
+                {
+
+                }
+              }
+            }
+            ////////// END DEPRECATED DIFFERENTIAL SETTING //////////
+            /// \brief imuTopic
+            ///
             std::string imuTopic;
             nhLocal_.getParam(imuTopicName, imuTopic);
 
