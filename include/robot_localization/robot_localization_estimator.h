5e55bf4fafaccd583d449629ddb8ac062a8912d9
Rokus Ottervanger <rokus.ottervanger@tno.nl>
2016-11-09 17:19:35 +0100

    Copy predict from EKF, not compiling

--- include/robot_localization/robot_localization_estimator.h
+++ include/robot_localization/robot_localization_estimator.h
@@ -35,6 +35,7 @@
 
 #include <Eigen/Dense>
 #include <boost/circular_buffer.hpp>
+#include "robot_localization/filter_utilities.h"
 
 #include <fstream>
 #include <vector>
@@ -62,6 +63,11 @@ struct EstimatorState
 
     //! @brief System input at time = time_stamp
     Eigen::VectorXd input;
+
+    friend std::ostream& operator<<(std::ostream &os, const EstimatorState& state)
+    {
+        return os << "state:\n - time_stamp: " << state.time_stamp;
+    }
 };
 
 //! @brief Robot Localization Listener class
@@ -78,7 +84,7 @@ class RobotLocalizationEstimator
     //! needed so that the ROS filters can pass arbitrary arguments to
     //! templated filter types).
     //!
-    explicit RobotLocalizationEstimator();
+    RobotLocalizationEstimator(unsigned int buffer_capacity);
 
     //! @brief Destructor for the RobotLocalizationListener class
     //!
@@ -97,7 +103,7 @@ class RobotLocalizationEstimator
     //!
     //! @param[out] state - The returned (most recent) state
     //!
-    int getState(EstimatorState &state);
+    int getState(EstimatorState &state) const;
 
     //! @brief Returns the state at a given time
     //!
@@ -107,11 +113,97 @@ class RobotLocalizationEstimator
     //! @param[in] time - The time at which the prediction is being made
     //! @param[out] state - The returned state at the given time
     //!
-    int getState(const double time, EstimatorState &state);
+    int getState(const double time, EstimatorState &state) const;
+
+    //! @brief Clears the internal state buffer
+    //!
+    void clearBuffer();
+
+    //! @brief Returns the buffer capacity
+    //!
+    unsigned int capacity() const;
+
+    //! @brief Returns the current buffer size
+    //!
+    unsigned int size() const;
 
   private:
 
     boost::circular_buffer<EstimatorState> state_buffer_;
+
+    friend std::ostream& operator<<(std::ostream &os, const RobotLocalizationEstimator& rle)
+    {
+      for ( boost::circular_buffer<EstimatorState>::const_iterator it = rle.state_buffer_.begin(); it != rle.state_buffer_.end(); ++it )
+      {
+        os << *it << "\n";
+      }
+      return os;
+    }
+
+    void extrapolate(const EstimatorState& boundary_state, const double requested_time, EstimatorState& state_at_req_time) const;
+
+    void interpolate(const EstimatorState& given_state_1, const EstimatorState& given_state_2, const double requested_time, EstimatorState& state_at_req_time) const;
+
+    //! @brief Gains applied to acceleration derived from control term
+    //!
+    std::vector<double> accelerationGains_;
+
+    //! @brief Caps the acceleration we apply from control input
+    //!
+    std::vector<double> accelerationLimits_;
+
+    //! @brief Variable that gets updated every time we process a measurement and we have a valid control
+    //!
+    Eigen::VectorXd controlAcceleration_;
+
+    //! @brief Gains applied to deceleration derived from control term
+    //!
+    std::vector<double> decelerationGains_;
+
+    //! @brief Caps the deceleration we apply from control input
+    //!
+    std::vector<double> decelerationLimits_;
+
+    //! @brief Which control variables are being used (e.g., not every vehicle is controllable in Y or Z)
+    //!
+    std::vector<int> controlUpdateVector_;
+
+    //! @brief Method for settings bounds on acceleration values derived from controls
+    //! @param[in] state - The current state variable (e.g., linear X velocity)
+    //! @param[in] control - The current control commanded velocity corresponding to the state variable
+    //! @param[in] accelerationLimit - Limit for acceleration (regardless of driving direction)
+    //! @param[in] accelerationGain - Gain applied to acceleration control error
+    //! @param[in] decelerationLimit - Limit for deceleration (moving towards zero, regardless of driving direction)
+    //! @param[in] decelerationGain - Gain applied to deceleration control error
+    //! @return a usable acceleration estimate for the control vector
+    //!
+    inline double computeControlAcceleration(const double state, const double control, const double accelerationLimit,
+      const double accelerationGain, const double decelerationLimit, const double decelerationGain)
+    {
+        const double error = control - state;
+        const bool sameSign = (::fabs(error) <= ::fabs(control) + 0.01);
+        const double setPoint = (sameSign ? control : 0.0);
+        const bool decelerating = ::fabs(setPoint) < ::fabs(state);
+        double limit = accelerationLimit;
+        double gain = accelerationGain;
+
+      if(decelerating)
+      {
+        limit = decelerationLimit;
+        gain = decelerationGain;
+      }
+
+      const double finalAccel = std::min(std::max(gain * error, -limit), limit);
+
+      return finalAccel;
+    }
+
+    void wrapStateAngles(EstimatorState state)
+    {
+      state.state(StateMemberRoll)  = FilterUtilities::clampRotation(state.state(StateMemberRoll));
+      state.state(StateMemberPitch) = FilterUtilities::clampRotation(state.state(StateMemberPitch));
+      state.state(StateMemberYaw)   = FilterUtilities::clampRotation(state.state(StateMemberYaw));
+    }
 
 };
 
