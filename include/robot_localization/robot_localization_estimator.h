679e13a9fb04096184d72343578d3e3c857190e0
Rokus Ottervanger <rokus.ottervanger@tno.nl>
2016-11-11 17:58:51 +0100

    Update docstrings

--- include/robot_localization/robot_localization_estimator.h
+++ include/robot_localization/robot_localization_estimator.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015, 2016, Charles River Analytics, Inc.
+ * Copyright (c) 2016, 2017, TNO IVS Helmond.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -109,6 +109,8 @@ class RobotLocalizationEstimator
     //!
     //! @param[out] state - The returned (most recent) state
     //!
+    //! @return false if buffer is empty, true otherwise
+    //!
     bool getState(EstimatorState &state) const;
 
     //! @brief Returns the state at a given time
@@ -119,6 +121,8 @@ class RobotLocalizationEstimator
     //! @param[in] time - The time at which the prediction is being made
     //! @param[out] state - The returned state at the given time
     //!
+    //! @return false if buffer is empty, true otherwise
+    //!
     bool getState(const double time, EstimatorState &state) const;
 
     //! @brief Clears the internal state buffer
@@ -128,13 +132,18 @@ class RobotLocalizationEstimator
     //! @brief Sets the buffer capacity
     //!
     //! @param[in] capacity - The new buffer capacity
+    //!
     void setBufferCapacity(const int capacity);
 
     //! @brief Returns the buffer capacity
     //!
+    //! @return buffer capacity
+    //!
     unsigned int capacity() const;
 
     //! @brief Returns the current buffer size
+    //!
+    //! @return current buffer size
     //!
     unsigned int size() const;
 
@@ -151,22 +160,32 @@ class RobotLocalizationEstimator
       return os;
     }
 
+    //! @brief Extrapolates the given state to a requested time stamp
+    //!
+    //! @param[in] boundary_state - state from which to extrapolate
+    //! @param[in] requested_time - time stamp to extrapolate to
+    //! @param[out] state_at_req_time - predicted state at requested time
+    //!
     void extrapolate(const EstimatorState& boundary_state, const double requested_time, EstimatorState& state_at_req_time) const;
 
+    //! @brief Interpolates the given state to a requested time stamp
+    //!
+    //! @param[in] given_state_1 - last state update before requested time
+    //! @param[in] given_state_2 - next state update after requested time
+    //! @param[in] requested_time - time stamp to extrapolate to
+    //! @param[out] state_at_req_time - predicted state at requested time
+    //!
     void interpolate(const EstimatorState& given_state_1, const EstimatorState& given_state_2, const double requested_time, EstimatorState& state_at_req_time) const;
 
-    //! @brief As we move through the world, we follow a predict/update
-    //! cycle. If one were to imagine a scenario where all we did was make
-    //! predictions without correcting, the error in our position estimate
-    //! would grow without bound. This error is stored in the
-    //! stateEstimateCovariance_ matrix. However, this matrix doesn't answer
-    //! the question of *how much* our error should grow for each time step.
-    //! That's where the processNoiseCovariance matrix comes in. When we
-    //! make a prediction using the transfer function, we add this matrix
-    //! (times deltaT) to the state estimate covariance matrix.
+    //! @brief When we make a prediction using the transfer function, we
+    //! add this matrix (times deltaT) to the state estimate covariance
+    //! matrix. This models the fact that the prediction is never perfect.
     //!
     Eigen::MatrixXd processNoiseCovariance_;
 
+    //! @brief Keeps the state's Roll, Pitch, Yaw angles in the range
+    //! [-pi, pi]
+    //!
     void wrapStateAngles(EstimatorState state) const
     {
       state.state(StateMemberRoll)  = FilterUtilities::clampRotation(state.state(StateMemberRoll));
