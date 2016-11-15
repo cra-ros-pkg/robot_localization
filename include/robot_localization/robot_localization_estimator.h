c72f20b6763c7b19d59f037692bdff4ef1fd5a5b
Rokus Ottervanger <rokus.ottervanger@tno.nl>
2016-11-15 10:58:04 +0100

    Update copyright notices in added files

--- include/robot_localization/robot_localization_estimator.h
+++ include/robot_localization/robot_localization_estimator.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2017, TNO IVS Helmond.
+ * Copyright (c) 2016, TNO IVS Helmond.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
