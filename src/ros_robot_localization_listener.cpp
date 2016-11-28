19b48d30b9f5ba9db3bfa842e00f6df53a084393
Rokus Ottervanger <rokus.ottervanger@tno.nl>
2016-11-28 10:24:36 +0100

    Fix(ros_robot_localization_listener): Catch tf lookup exception

--- src/ros_robot_localization_listener.cpp
+++ src/ros_robot_localization_listener.cpp
@@ -183,10 +183,18 @@ namespace RobotLocalization
 
     // First get the transform from base to target
     geometry_msgs::TransformStamped base_to_target_transform;
-    base_to_target_transform = tf_buffer_.lookupTransform(frame_id,
-                                                          base_frame_id_,
-                                                          ros::Time(time),
-                                                          ros::Duration(0.1)); // TODO: magic number
+    try
+    {
+      base_to_target_transform = tf_buffer_.lookupTransform(frame_id,
+                                                            base_frame_id_,
+                                                            ros::Time(time),
+                                                            ros::Duration(0.1)); // TODO: magic number
+    }
+    catch ( tf2::LookupException e )
+    {
+      ROS_WARN_STREAM("Ros Robot Localization Listener: Could not look up transform: " << e.what());
+      return false;
+    }
 
     // And convert it to an eigen Affine transformation
     Eigen::Affine3d target_pose_base;
