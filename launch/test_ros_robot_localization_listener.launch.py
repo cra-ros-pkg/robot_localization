14620f564a00ee374c335e7613fa8b08de4e7d4d
Rokus Ottervanger <rokus.ottervanger@tno.nl>
2016-11-29 17:20:40 +0100

    Add(test): Ros Robot Localization Listener test

---
+++ launch/test_ros_robot_localization_listener.launch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import ament_index_python.packages
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+
+  # TODO: Port ROS 1 test launch params: clear_params="true" time-limit="100.0"
+  return launch.LaunchDescription([
+    launch_ros.actions.Node(
+      package='robot_localization',
+      node_executable='test_ros_robot_localization_listener',
+    ),
+  ])
