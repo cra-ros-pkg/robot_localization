e26ea32d847822277389e1e3a1cfeeb817ab1030
Tom Moore <tmoore@locusrobotics.com>
2018-06-27 16:36:02 +0100

    Removing tests and launch for now

--- test/record_all_stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./stat_recorder.sh test_ekf_localization_node_bag1.test $1/ekf1.txt
-./stat_recorder.sh test_ekf_localization_node_bag2.test $1/ekf2.txt
-./stat_recorder.sh test_ekf_localization_node_bag3.test $1/ekf3.txt
-
-./stat_recorder.sh test_ukf_localization_node_bag1.test $1/ukf1.txt
-./stat_recorder.sh test_ukf_localization_node_bag2.test $1/ukf2.txt
-./stat_recorder.sh test_ukf_localization_node_bag3.test $1/ukf3.txt
