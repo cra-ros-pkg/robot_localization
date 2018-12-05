43cfd881eb109e2e93582876043f61a167eb33e0
rohit_agrawal1 <rohit_agrawal1@persistent.co.in>
2018-12-05 18:32:30 +0530

    Added test_ukf_localization_node_interfaces with ROS2 format launch.py & .yaml

---
+++ launch/test_ukf_localization_node_interfaces.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    parameters_file_dir = pathlib.Path(__file__).resolve().parent
+    parameters_file_path = parameters_file_dir /'test_ukf_localization_node_interfaces.yaml'    
+    os.environ['FILE_PATH'] = str(parameters_file_dir)
+    return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'output_final_position',
+            default_value='false'),
+	
+	 #*****test_ukf_localization_node_interfaces.test***** 
+	launch_ros.actions.Node(
+            package='robot_localization', node_executable='se_node', node_name='test_ukf_localization_node_interfaces_ukf',
+	    output='screen',
+	    parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'test_ukf_localization_node_interfaces.yaml'],
+           ],	
+	   ),
+        launch_ros.actions.Node(
+            package='robot_localization', node_executable='test_ukf_localization_node_interfaces',node_name='test_ukf_localization_node_interfaces_int',
+            output='screen',
+      	    parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'test_ukf_localization_node_interfaces.yaml'],
+           ],
+	   ),
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
