862d447ac9afc65d694246c76082368d65cf9f25
rohit_agrawal1 <rohit_agrawal1@persistent.co.in>
2018-12-20 19:37:12 +0530

    Rename file names to lower-case.

--- test/Robot_Localization_Testcases.sh
+++
@@ -1,3 +0,0 @@
-
-source ~/rl_ws/install/setup.bash
-/usr/bin/python3 Robot_Localization_Testcases.py
---
+++ test/robot_localization_test_cases.sh
@@ -0,0 +1,3 @@
+
+source ~/rl_ws/install/setup.bash
+/usr/bin/python3 robot_localization_test_cases.py
