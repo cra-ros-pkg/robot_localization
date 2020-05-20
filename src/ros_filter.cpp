a7759211a028deea6e767a7a4747b076716fed73
florianspy <florian.spiess@gmx.net>
2020-05-20 14:35:35 +0200

    unsilent

--- src/ros_filter.cpp
+++ src/ros_filter.cpp
@@ -2402,13 +2402,13 @@ namespace RobotLocalization
     // we have to handle the situation.
     tf2::Transform targetFrameTrans;
     bool silent_tf_failure;
-    nh_.getParam("/silent_tf_failure",silent_tf_failure);
+    nh_.getParam("/silent_tf_failure", silent_tf_failure);
     bool canTransform = RosFilterUtilities::lookupTransformSafe(tfBuffer_,
                                                                  targetFrame,
                                                                  msgFrame,
                                                                  msg->header.stamp,
-                                                                 tfTimeout_, 
-                                                                  targetFrameTrans, 
+                                                                 tfTimeout_,
+                                                                  targetFrameTrans,
                                                                   silent_tf_failure);
 
     if (canTransform)
