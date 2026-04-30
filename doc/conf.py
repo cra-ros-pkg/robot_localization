a63145ac904cc93ee553d0cfb8746406d1b4264d
Tom Moore <tmoore@locusrobotics.com>
2026-04-30 10:58:15 +0100

    Fixing rosdoc (#970)
    * Fixing rosdoc

--- doc/conf.py
+++ doc/conf.py
@@ -1,10 +1,6 @@
 import sys
 import os
 
-import catkin_pkg.package
-catkin_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-catkin_package = catkin_pkg.package.parse_package(os.path.join(catkin_dir, catkin_pkg.package.PACKAGE_MANIFEST_FILENAME))
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
@@ -14,17 +10,18 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
-templates_path = ['../.templates']
+templates_path = ['.templates']
 source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'robot_localization'
 copyright = u'2016, Tom Moore'
 author = u'Tom Moore'
-version = catkin_package.version
-release = catkin_package.version
+# version/release are provided by rosdoc2 from package.xml; keep in sync manually for local builds.
+version = '3.10.0'
+release = '3.10.0'
 
-language = None
+language = 'en'
 exclude_patterns = ['.build']
 pygments_style = 'sphinx'
 todo_include_todos = True
@@ -37,7 +34,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../images/rl_small.png'
+html_logo = 'images/rl_small.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -194,4 +191,4 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
